2ac4b8302b4e788f511a196772dcd74ddb26dd15
vrastello <vincent.rastello@spin.pm>
2025-04-25 17:21:15 -0700

    use tap instead

--- app/controllers/api/game_events_controller.rb
+++ app/controllers/api/game_events_controller.rb
@@ -1,7 +1,6 @@
 
 class GameEventsController < ApplicationController
   def create
-
     @game_event = @current_user.game_events.new(game_event_params)
     if @game_event.save
       render json: @game_event, status: :created
@@ -13,7 +12,8 @@ class GameEventsController < ApplicationController
   private
 
   def game_event_params
-    event_params = params.require(:game_event).permit(:game_name, :type, :occured_at)
-    event_params["event_type"] = event_params.delete("type") if event_params["type"]
+    params.require(:game_event).permit(:game_name, :type, :occured_at).tap do |whitelisted|
+      whitelisted[:event_type] = whitelisted.delete(:type) if whitelisted[:type]
+    end
   end
 end
